5d0c73880728941758e317e902912a4e55587659
Jonas Herminio <jonasherminio@live.com>
2020-12-03 00:07:04 -0300

    Novas Questões

--- Planilhas/05.py
+++ Planilhas/05.py
@@ -3,14 +3,12 @@ from openpyxl import load_workbook
 # https://letscode.com.br/blog/aprenda-a-integrar-python-e-excel
 # https://openpyxl.readthedocs.io/en/stable/tutorial.html
 
-# caminho = 'AULAS-EAD-REGISTRO-DE-ATIVIDADE.xlsx'
 # lendo a planilha
 caminho = 'Aprendendoalerplanilhas.xlsx'
 # abrindo
 arquivo_excel = load_workbook(caminho, data_only=True)
 # setando na aba da planilha
 planilha = arquivo_excel.active
-# apontando na linha e coluna
 sim = 0
 nao = 0
 #a coluna para ficar linear nos cabeçalhos
@@ -18,9 +16,9 @@ coluna =5
 #para não repetir o cod com "Aluno 1,2,3,4 ec..."
 for y in range(1, 11): #Para de aluno 1 até aluno 10
     aluno = ["Aluno"]
-    alunonum = [y] #aluno recebe 1,2,3.....10
+    alunonum = [y] #alunonum recebe 1,2,3.....10
     #str(alunonum)
-    aluno += alunonum #concatenação das listas
+    aluno += alunonum #concatenação das listas [Aluno] + [1]
     uniao = " ".join([str(_) for _ in aluno]) #transformando srt + int em string e tirando os [] etc...
     # sem isso ele ficaria assim: ['Aluno', 1] e não daria certo nas condicionais
     str(uniao) #convertendo pra string e ficará normal =======> Aluno 1
@@ -33,7 +31,7 @@ for y in range(1, 11): #Para de aluno 1 até aluno 10
 
         elif conteudo.value == uniao and ponteirocont.value == "NÃO":
             nao += 1
-    #saindo do laço 2 > aqui add as informações nas colunas
+    #saindo do for 2 > aqui add as informações nas colunas
     planilha.cell(row=7, column=coluna, value=uniao)
     planilha.cell(row=8, column=coluna, value=sim)
     planilha.cell(row=9, column=coluna, value=nao)
